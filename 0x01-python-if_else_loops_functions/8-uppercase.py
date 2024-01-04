53d33b0a7c8ba6e40e55768b066a001e3e635be7
Bonito <bonito@gmail.com>
2024-01-03 18:58:20 -0800

    Changes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(s):
         if ord('a') <= ord(char) <= ord('z'):
             print("{:c}".format(ord(char) - 32), end="")
         else:
-            print("{:c}".format(ord(char)), end=" ")
+            print("{:c} ".format(ord(char)), end="")
